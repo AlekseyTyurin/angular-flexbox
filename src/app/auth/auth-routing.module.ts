e9af5aa8ebd2738d31ebd173ae62985cc5874da2
ALEXEY TYURIN <ALEXGEOLOG@GMAIL.COM>
2017-09-03 20:01:13 +0300

    - created three main encapsulated components:
    1. Created Login and SignUp subcomponent in the Auth Component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,13 +1,17 @@
 import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AuthComponent} from "./auth.component";
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
 
 // const routes: Routes = [
 //     {path: '', component: AuthComponent}
 // ];
 
 const authRoutes: Routes = [
-    {path: '', component: AuthComponent}
+    {path: '', component: AuthComponent},
+    {path: 'auth/login', component: LoginComponent},
+    {path: 'auth/signup', component: SignupComponent}
 ];
 
 export const authRouting: ModuleWithProviders = RouterModule.forRoot(authRoutes);
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,15 +3,21 @@ import {CommonModule} from "@angular/common";
 import {authRouting} from "./auth-routing.module";
 import {AuthComponent} from "./auth.component";
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import {LoginModule} from "./login/login.module";
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        LoginModule,
         authRouting
     ],
     declarations: [
         AuthComponent,
-        FooterComponent
+        FooterComponent,
+        LoginComponent,
+        SignupComponent
     ]
 })
 export class AuthModule {
